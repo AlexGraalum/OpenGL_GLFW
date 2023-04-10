fb2ca79b8f317a64f22b8ce372a3c2939168416b
graalumj <alexgraalum@yahoo.com>
2023-04-10 15:25:18 -0700

    Cleaned unused GLFWwindow and added Defines

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,3 @@
-//#include <glad\glad.h>
-//#include <GLFW\glfw3.h>
-
-#define WINDOWHEADER
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
@@ -25,6 +20,10 @@
 
 #include <string>
 
+//DEFINES
+#define MINSPIN 5.0f
+#define MAXSPIN 50.0f
+
 //PROTOTYPES
 void framebuffer_size_callback(GLFWwindow*, int, int);
 void keyCallback(GLFWwindow*, int, int, int, int);
@@ -34,11 +33,7 @@ void processInput();
 void Animate();
 
 //GLOBALS
-#ifdef WINDOWHEADER
 std::shared_ptr<Window> windowPtr;
-#else
-GLFWwindow* windowPtr;
-#endif
 std::shared_ptr<Camera> camera;
 
 float deltaTime = 0.0f;
@@ -81,7 +76,7 @@ const glm::vec3 pyramidScale[] = {
 ////2D Objects
 //Square
 std::vector<Vertex> squareVertices = {
-     //position                                      normal                       texture
+     //               position                       normal                       texture
      Vertex(glm::vec3(-0.5f,  0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 1.0f)),
      Vertex(glm::vec3(-0.5f, -0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 0.0f)),
      Vertex(glm::vec3( 0.5f, -0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(1.0f, 0.0f)),
@@ -127,33 +122,7 @@ const glm::vec2 triangleScale[] = {
 int main(int argc, char** argv) {
 
      //Create Window
-#ifdef WINDOWHEADER
      windowPtr = Window::CreateWindow("Learn OpenGL", SCR_WIDTH, SCR_HEIGHT, framebuffer_size_callback, keyCallback);
-#else
-     glfwInit();
-     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-     
-     window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Learn OpenGL", NULL, NULL);
-     if (window == NULL) {
-          fprintf(stderr, "Failed to create GLFW window\n");
-          glfwTerminate();
-          return -1;
-     }
-     
-     glfwMakeContextCurrent(window);
-     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-     glfwSetKeyCallback(window, keyCallback);
-     
-     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-          fprintf(stderr, "Failed to initialize GLAD\n");
-          return -1;
-     }
-#endif
-
-
 
      //Create Camera
      camera = Camera::CreateCamera(glm::vec3(0.0f, 0.0f, 6.0f),
@@ -183,11 +152,7 @@ int main(int argc, char** argv) {
      lightPos = glm::vec3(0.0f);
 
      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-#ifdef WINDOWHEADER
      while (!windowPtr->ShouldClose()) {
-#else
-     while(!glfwWindowShouldClose(window)){
-#endif
           float currentFrame = static_cast<float>(glfwGetTime());
           deltaTime = currentFrame - lastFrame;
           lastFrame = currentFrame;
@@ -203,11 +168,11 @@ int main(int argc, char** argv) {
           //Render 3D Scene
           cubeMesh->Draw3D(lightPos, glm::vec4(1.0f, 0.0f, 0.0f, 0.0f), glm::vec3(0.1f), camera, { unLitCubeShader }, { NULL }, {});
 
-          cubeMesh->Draw3D(   glm::vec3( 1.0f,  1.0f,  0.0f), glm::vec4(0.0f, 1.0f, 0.0f, angle), glm::vec3(0.5f), camera, { litCubeShader }, { NULL }, lightPos);
-          pyramidMesh->Draw3D(glm::vec3( 1.0f, -1.0f,  0.0f), glm::vec4(0.0f, 1.0f, 0.0f, angle), glm::vec3(0.5f), camera, { litCubeShader }, { texture }, lightPos);
+          cubeMesh->Draw3D(   glm::vec3( 1.0f,  1.0f,  0.0f), glm::vec4(0.0f, 1.0f, 0.0f, angle), glm::vec3(0.5f), camera, { litCubeShader }, { NULL },       lightPos);
+          pyramidMesh->Draw3D(glm::vec3( 1.0f, -1.0f,  0.0f), glm::vec4(0.0f, 1.0f, 0.0f, angle), glm::vec3(0.5f), camera, { litCubeShader }, { texture },    lightPos);
           cubeMesh->Draw3D(   glm::vec3(-1.0f, -1.0f,  0.0f), glm::vec4(0.0f, 1.0f, 0.0f, angle), glm::vec3(0.5f), camera, { litCubeShader }, { catCubeTex }, lightPos);
 
-          shipMesh->Draw3D(   glm::vec3(-1.0f,  1.0f,  0.0f), glm::vec4(0.0f, 1.0f, 0.0f, angle), glm::vec3(1.0f), camera, {litCubeShader}, { NULL }, lightPos);
+          shipMesh->Draw3D(   glm::vec3(-1.0f,  1.0f,  0.0f), glm::vec4(0.0f, 1.0f, 0.0f, angle), glm::vec3(1.0f), camera, {litCubeShader},   { NULL },       lightPos);
 
           //Render 2D Scene
           for (unsigned int i = 0; i < (sizeof(squarePos) / sizeof(squarePos[0])); i++) {
@@ -217,18 +182,9 @@ int main(int argc, char** argv) {
           cubeMesh->Draw3DIn2D(glm::vec3(-0.5f, 0.5f, 0.0f), glm::vec4(1.0f, 1.0f, 0.0f, angle), glm::vec3(0.1f), { unLitCubeShader }, { catCubeTex }, {});
 
           //Poll Events and Swap Buffers
-#ifdef WINDOWHEADER
           windowPtr->Flush();
-#else
-          glfwSwapBuffers(window);
-          glfwPollEvents();
-#endif
      }
 
-#ifndef WINDOWHEADER
-     glfwDestroyWindow(window);
-     glfwTerminate();
-#endif
      return 0;
 }
 
@@ -237,16 +193,46 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 }
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-     if (key == GLFW_KEY_R && action == GLFW_PRESS) spin = !spin;
-     if (key == GLFW_KEY_EQUAL && action == GLFW_PRESS && angleIncrement < 15.0f) angleIncrement += 1.0f;
-     if (key == GLFW_KEY_MINUS && action == GLFW_PRESS && angleIncrement > 5.0f) angleIncrement -= 1.0f;
+     if (action == GLFW_PRESS) {
+          switch (key) {
+          //Spin Toggle
+          case GLFW_KEY_R:
+               spin = !spin;
+               break;
+
+          //Spin Speed Increase
+          case GLFW_KEY_EQUAL:
+               if (angleIncrement < MAXSPIN) {
+                    if (mods == GLFW_MOD_SHIFT && MAXSPIN - angleIncrement >= 5.0f) {
+                         angleIncrement += 5.0f;
+                    } else {
+                         angleIncrement += 1.0f;
+                    }
+                    windowPtr->UpdateTitle("SpinSpeed = " + std::to_string(angleIncrement));
+               }
+               break;
+          //Spin Speed Decrease
+          case GLFW_KEY_MINUS:
+               if (angleIncrement > MINSPIN) {
+                    if (mods == GLFW_MOD_SHIFT && angleIncrement - MINSPIN >= 5.0f) {
+                         angleIncrement -= 5.0f;
+                    } else {
+                         angleIncrement -= 1.0f;
+                    }
+                    windowPtr->UpdateTitle("SpinSpeed = " + std::to_string(angleIncrement));
+               }
+               break;
+          }
+     }
 }
 
 void processInput() {
-#ifdef WINDOWHEADER
+     //Close Window
      if (windowPtr->ProcessInput(GLFW_KEY_ESCAPE)) {
           windowPtr->Close();
      }
+
+     //Camera Control
      if (windowPtr->ProcessInput(GLFW_KEY_W)) camera->MoveFront(deltaTime);
      if (windowPtr->ProcessInput(GLFW_KEY_S)) camera->MoveBack(deltaTime);
      if (windowPtr->ProcessInput(GLFW_KEY_A)) camera->MoveLeft(deltaTime);
@@ -254,31 +240,13 @@ void processInput() {
      if (windowPtr->ProcessInput(GLFW_KEY_SPACE)) camera->MoveUp(deltaTime);
      if (windowPtr->ProcessInput(GLFW_KEY_LEFT_CONTROL)) camera->MoveDown(deltaTime);
 
+     //Light Source Control
      if (windowPtr->ProcessInput(GLFW_KEY_J)) { lightPos += (deltaTime * 10.0f) * glm::vec3(1.0f, 0.0f, 0.0f); }
      if (windowPtr->ProcessInput(GLFW_KEY_L)) { lightPos -= (deltaTime * 10.0f) * glm::vec3(1.0f, 0.0f, 0.0f); }
      if (windowPtr->ProcessInput(GLFW_KEY_K)) { lightPos -= (deltaTime * 10.0f) * glm::vec3(0.0f, 0.0f, 1.0f); }
      if (windowPtr->ProcessInput(GLFW_KEY_I)) { lightPos += (deltaTime * 10.0f) * glm::vec3(0.0f, 0.0f, 1.0f); }
      if (windowPtr->ProcessInput(GLFW_KEY_U)) { lightPos -= (deltaTime * 10.0f) * glm::vec3(0.0f, 1.0f, 0.0f); }
      if (windowPtr->ProcessInput(GLFW_KEY_O)) { lightPos += (deltaTime * 10.0f) * glm::vec3(0.0f, 1.0f, 0.0f); }
-#else
-     if (glfwGetKey(windowPtr, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-          glfwSetWindowShouldClose(windowPtr, true);
-     }
-
-     if (glfwGetKey(windowPtr, GLFW_KEY_W) == GLFW_PRESS) camera->MoveFront(deltaTime);
-     if (glfwGetKey(windowPtr, GLFW_KEY_S) == GLFW_PRESS) camera->MoveBack(deltaTime);
-     if (glfwGetKey(windowPtr, GLFW_KEY_A) == GLFW_PRESS) camera->MoveLeft(deltaTime);
-     if (glfwGetKey(windowPtr, GLFW_KEY_D) == GLFW_PRESS) camera->MoveRight(deltaTime);
-     if (glfwGetKey(windowPtr, GLFW_KEY_SPACE) == GLFW_PRESS) camera->MoveUp(deltaTime);
-     if (glfwGetKey(windowPtr, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) camera->MoveDown(deltaTime);
-
-     if (glfwGetKey(windowPtr, GLFW_KEY_J) == GLFW_PRESS)   { lightPos += (deltaTime * 10.0f) * glm::vec3( 1.0f, 0.0f, 0.0f); }
-     if (glfwGetKey(windowPtr, GLFW_KEY_L) == GLFW_PRESS)   { lightPos -= (deltaTime * 10.0f) * glm::vec3( 1.0f, 0.0f, 0.0f); }
-     if (glfwGetKey(windowPtr, GLFW_KEY_K) == GLFW_PRESS)   { lightPos -= (deltaTime * 10.0f) * glm::vec3( 0.0f, 0.0f, 1.0f); }
-     if (glfwGetKey(windowPtr, GLFW_KEY_I) == GLFW_PRESS)   { lightPos += (deltaTime * 10.0f) * glm::vec3( 0.0f, 0.0f, 1.0f); }
-     if (glfwGetKey(windowPtr, GLFW_KEY_U) == GLFW_PRESS)   { lightPos -= (deltaTime * 10.0f) * glm::vec3( 0.0f, 1.0f, 0.0f); }
-     if (glfwGetKey(windowPtr, GLFW_KEY_O) == GLFW_PRESS)   { lightPos += (deltaTime * 10.0f) * glm::vec3( 0.0f, 1.0f, 0.0f); }
-#endif
 }
 
 void Animate() {
